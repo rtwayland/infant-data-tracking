25778c79fdd807615dac596a9c09e0c52899c0fc
raleigh wayland <rtwayland@gmail.com>
2017-01-28 22:23:37 -0700

    Remove log statement

--- public/js/controllers/NapDataController.js
+++ public/js/controllers/NapDataController.js
@@ -7,7 +7,7 @@ angular.module('app')
         $scope.getNapData = function() {
             NapService.getNaps()
                 .then(function(response) {
-                    console.log('Nap Arr Data\n', response);
+                    // console.log('Nap Arr Data\n', response);
                     $scope.naps = response;
                 }, function(error) {
                     console.log(error);
